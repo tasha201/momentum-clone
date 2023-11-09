5f6a50bbfdc5df5593b411765f1f807edeb69829
tasha201 <natalia.it.designer@gmail.com>
2023-11-09 20:35:17 +0200

    Updating crypto block

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-  .then(res => res.json())
-  .then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
   })
-  .catch(err => {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+    <img src=${data.image.small} />
+    <span>${data.name}</span>
+    `;
+    document.getElementById("crypto").innerHTML += `
+      <p>🎯: $${data.market_data.current_price.usd}</p>
+      <p>👆: $${data.market_data.high_24h.usd}</p>
+      <p>👇: $${data.market_data.low_24h.usd}</p>
+    `;
+  })
+  .catch((err) => console.error(err));
