d0de4743d908dbfe103f23f7508401e526a15480
tasha201 <natalia.it.designer@gmail.com>
2023-11-12 22:53:06 +0200

    Adding template for receipt card

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `Image by: ${data.user.name}`;
+    document.getElementById(
+      "author"
+    ).textContent = `Image by: ${data.user.name}`;
   })
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
     document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
 
-  // fetching bitcoin data
+// fetching bitcoin data
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
   .then((res) => {
     if (!res.ok) {
@@ -33,14 +35,19 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
       <p>👇: $${data.market_data.low_24h.usd}</p>
     `;
     }
-    document.getElementById("crypto-top").addEventListener("click", handleClick);
+    document
+      .getElementById("crypto-top")
+      .addEventListener("click", handleClick);
   })
   .catch((err) => console.error(err));
 
 // get and display current time
 function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
 }
 setInterval(getCurrentTime, 1000);
 
@@ -56,27 +63,27 @@ navigator.geolocation.getCurrentPosition((position) => {
       return res.json();
     })
     .then((data) => {
-      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
                 <p class="weather-city">${data.name}</p>
-            `
+            `;
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 });
 
 // quote generator
 async function getQuote() {
   try {
     await fetch("https://api.quotable.io/random")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // console.log(data)
-        document.getElementById("quote").innerText =`"${data.content}"`;
+        document.getElementById("quote").innerText = `"${data.content}"`;
       });
   } catch (error) {
-      console.error("Error:", error);
+    console.error("Error:", error);
   }
 }
 window.addEventListener("load", getQuote);
@@ -98,13 +105,23 @@ if (hours >= 4 && hours <= 12) {
 function handleInputChange(event) {
   document.getElementById("focus").value = event.target.value;
   //  console.log(event.target.value)
-  document.getElementById("focus").style.display = "none"
+  document.getElementById("focus").style.display = "none";
   document.getElementById("message").textContent = event.target.value;
 }
 
 function editing() {
-  document.getElementById("focus").style.display = "block"
+  document.getElementById("focus").style.display = "block";
 }
 
-document.getElementById("focus").addEventListener("change", handleInputChange)
-document.getElementById("message").addEventListener("click", editing)
+document.getElementById("focus").addEventListener("change", handleInputChange);
+document.getElementById("message").addEventListener("click", editing);
+
+// receipt card
+function handleClickOnReceipt() {
+  document.getElementById("receipt").style.display = "none";
+  document.getElementById("flip-card").style.display = "block";
+}
+
+document
+  .getElementById("receipt")
+  .addEventListener("click", handleClickOnReceipt);
